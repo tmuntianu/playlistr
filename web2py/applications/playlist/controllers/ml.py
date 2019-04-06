06a619f24f3f582d681ca4ef7d3a8022cc6c1f6e
Sam Schiff <samuel.b.schiff.20@dartmouth.edu>
2019-04-06 13:39:32 -0400

    changed structure

--- web2py/applications/playlist/controllers/ml.py
+++ web2py/applications/playlist/controllers/ml.py
@@ -1,9 +1,21 @@
+
+
+def getTopTracks(tokens):
+
+
+
+
+
+
+
+
+
+
 def generatePlaylists(ownerToken, tokens):
-	tracks = []
-	for token in tokens:
-		tracks.extend(getTopTracks(token)) #put all the top tracks into one list
+	tracks = getTopTracks(tokens) #put all the top tracks into one list
 	genres = getTopGenres(tracks) # get the top 3 genres
 	tracksByGenre = getTracksByGenre(tracks, genres) #return dictionary of genre names to tracks
 	featureVectors = getFeatureVectors(tracksByGenre) #returns a dictionary of genre names to feature vectors
 	playlists = getTracksFromSeed(featureVectors) #returns a dictionary of genre name to track array
 	createPlaylists(ownerToken, playlists) #creates playlists in the owner's account
+
