cb0c173063310f60be9536e7835ee8eec26c6a17
Sam Schiff <samuel.b.schiff.20@dartmouth.edu>
2019-04-06 13:49:54 -0400

    added sps

--- web2py/applications/playlist/controllers/ml.py
+++ web2py/applications/playlist/controllers/ml.py
@@ -1,6 +1,6 @@
+import spotipy
 
-
-def getTopTracks(tokens):
+def getTopTracks(sps):
 
 
 
@@ -12,10 +12,18 @@ def getTopTracks(tokens):
 
 
 def generatePlaylists(ownerToken, tokens):
-	tracks = getTopTracks(tokens) #put all the top tracks into one list
+	sps = authSpotipy(tokens)
+	tracks = getTopTracks(sps) #put all the top tracks into one list
 	genres = getTopGenres(tracks) # get the top 3 genres
 	tracksByGenre = getTracksByGenre(tracks, genres) #return dictionary of genre names to tracks
 	featureVectors = getFeatureVectors(tracksByGenre) #returns a dictionary of genre names to feature vectors
 	playlists = getTracksFromSeed(featureVectors) #returns a dictionary of genre name to track array
 	createPlaylists(ownerToken, playlists) #creates playlists in the owner's account
 
+
+
+def authSpotipy(tokens):
+	sps = []
+	for token in tokens
+		sps.append(spotipy.Spotify(auth=token))
+	return sps
