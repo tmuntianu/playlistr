e0080225762025a96d594db508fd92e0ffea98d3
tmuntianu <tmuntia@yahoo.com>
2019-04-06 22:50:14 -0400

    finished basic frontend

--- web2py/applications/init/controllers/default.py
+++ web2py/applications/init/controllers/default.py
@@ -52,13 +52,39 @@ def auth_success():
     owner_email = user.email
     form = SQLFORM(db.sp_group, labels = {'sp_member':'Emails'})
 
-    if form.validate():
+    if form.validate() and form.vars.sp_member:
         for row in db(db.sp_group.sp_owner == owner_email).select():
             row.delete_record()
         db.sp_group.insert(sp_owner=owner_email, sp_member=form.vars.sp_member)
         return redirect(URL('makelist'))
 
     return dict(form=form)
+
+@auth.requires_login()
+def makelist():
+    owner_email = db.auth_user(id=auth.user_id).email
+    emails = []
+    for row in db(db.sp_group.sp_owner == owner_email).select():
+        emails = (row.sp_member)
+    emails.insert(0, owner_email)
+    tokens = []
+    for email in emails:
+        acct_exists = False
+        refr_token = ''
+        for row in db(db.auth_user.email == email).select():
+            refr_token = row.sp_refresh_token
+            acct_exists = True
+        if acct_exists:
+            scope = 'user-top-read playlist-modify-public'
+            client_id="22afe11d6c9a4302804622924738a872"
+            client_secret="f6ab191a59de4b59af13dc44d7ec16c5"
+            redirect_uri="http://127.0.0.1:8000/init/default/auth_success"
+            oauth = spotipy.oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scope)
+            new_token = oauth.refresh_access_token(refr_token)
+            tokens.append(new_token['access_token'])
+        
+    return dict(tokens = tokens)
+
 
 # ---- action to server uploaded static content (required) ---
 @cache.action()
